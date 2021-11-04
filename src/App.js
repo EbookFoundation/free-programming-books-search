cc8d04154e83090e583a9b3cc217da3b72471048
Dylan Regan <4dylanregan@gmail.com>
2021-11-03 22:19:18 -0400

    please work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+import React, { Component } from 'react';
+ 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        message: "Default Content"
+    }
+  }
+ 
+  updateContent = () => {
+    if (this.state.message == "Default Content") {
+      this.setState({ message: "Updated Content!"});
+    }
+    else {
+      this.setState({ message: "Default Content"});
+    }
+  }
+ 
+  render() {
+    return (
+      <div>
+        <div className="h1 bg-secondary text-white text-center p-2">
+          { this.state.message }
+        </div>
+        <div className="text-center">
+          <button className="btn btn-secondary" onClick={this.updateContent}>
+            Click Me
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
