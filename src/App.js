f1fab3892d9ccc1de507b48bf2e01ca480e4d2a8
Dylan Regan <4dylanregan@gmail.com>
2021-11-03 21:24:36 -0400

    test

--- src/App.js
+++ src/App.js
@@ -3,22 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          why does this not work
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I DID
-        </a>
-      </header>
-    </div>
+    <h1>Hello World</h1>
   );
 }
 
