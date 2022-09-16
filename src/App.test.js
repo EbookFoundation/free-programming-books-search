d04043460bb002840d421e255c97cab8dc166642
David Ordás <3125580+davorpa@users.noreply.github.com>
2022-09-16 12:07:11 +0200

    fix: App.js testcases
    it already holds the inital code :worried:

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders EbookFoundation/free-programming-books link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    /EbookFoundation\/free-programming-books/i
+  );
   expect(linkElement).toBeInTheDocument();
 });
