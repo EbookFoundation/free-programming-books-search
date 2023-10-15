be35d1ad3f7086901a45dcee20ac52d35bfd1c3c
Chung Yan Ho <100369959+hochungyan@users.noreply.github.com>
2023-10-15 02:46:00 +0100

    Added 1 Unit Test in App.test.js
    Added a unit test for renders header text

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders header text', () => {
+  render(<App />);
+  const headerElement = screen.getByText(/free-programming-books/i);
+  expect(headerElement).toBeInTheDocument();
+});
